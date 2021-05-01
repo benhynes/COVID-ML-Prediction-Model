1c9d73472c51631f3bb2478822d8653833e09af3
benhynes <zulut3ch@gmail.com>
2021-05-01 13:44:15 -0700

    Updated for parsing past data

--- utils/csv_CNN.py
+++ utils/csv_CNN.py
@@ -18,3 +18,21 @@ def parseToCSV(ans):
                     rowTemp.append(ans[date][row][item])
                 writer.writerow(rowTemp)
                 rowTemp = []
+                
+def parsePastToCSV(past):
+    dirname = os.path.dirname(__file__)
+    for date in range(len(past), 1):
+        filename = os.path.join(dirname, '../frontend/forecast/p{}.csv'.format(date))
+        with open(filename, 'w', newline='') as file:
+            writer = csv.writer(file)
+            
+            rowTemp = []
+            
+            rowSize = past[date].shape[0]
+            colSize = past[date].shape[1]
+            
+            for row in range(rowSize):
+                for item in range(colSize):
+                    rowTemp.append(past[date][row][item])
+                writer.writerow(rowTemp)
+                rowTemp = []
