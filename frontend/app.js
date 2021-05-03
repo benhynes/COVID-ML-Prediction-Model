2a14efdfda9321c799d2d064bb4fc8dbb1f810ab
rmp9 <57851775+raizapw19@users.noreply.github.com>
2021-05-02 19:33:43 -0700

    Update lineChart options

--- frontend/app.js
+++ frontend/app.js
@@ -3,10 +3,12 @@ const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
 const DATE_TODAY = new Date();
 const MILLISECONDS_IN_A_DAY = 86400000;
 google.charts.load('46', {'packages':['corechart']});
-//google.charts.setOnLoadCallback(drawChart);
+var slider_value;
+var slider_selected_date;
 
 /**
- * Reads a csv file, assign values (weights) to respective latitude and longitude,
+ * Reads csv files for past, current and future active cases,
+ * assign values (weights) to respective latitude and longitude,
  * and stores it as LatLng object to the points array.
  * @return Array of points from day 0 to day 9
 */
@@ -40,12 +42,250 @@ async function getData() {
 }
 
 /**
+ * Computes the total number of cases in a day and stores them into an
+ * array of days with their respective total number of cases
+ * @param days_array, array of number of cases per affected location per day
+ */
+function getTotalNumberArray(days_array) {
+  var total_number_array = [];
+
+  for (var i = 0; i < days_array.length; i++) {
+    var total_number_of_cases = 0;
+
+    for (var j = 0; j < days_array[i].length; j++) {
+      total_number_of_cases += parseFloat(days_array[i][j]['weight']);
+    }
+
+    total_number_array.push(total_number_of_cases);
+  }
+
+  return total_number_array;
+}
+
+/**
+ * Stores number of cases per day into their respective location
+ * @param days_array, array of days with information about locations
+ *      and number of cases
+ * @returns 2d array of locations and their respective number of cases
+ *      in the span of 19 days
+ */
+function getDataPerLocation(days_array) {
+  var location;
+  var weight;
+  var locations = [];
+
+  //Iterate through day 0 to 18
+  for (var i = 0; i < days_array.length; i++) {
+
+    //Iterate through every location
+    for (var j = 0; j < days_array[i].length; j++) {
+      location = [days_array[i][j]['location'].lat() + " " + days_array[i][j]['location'].lng()];
+      weight = days_array[i][j]['weight'];
+      var index = -1;
+      var k;
+
+      //Check if location is in locations array
+      for (k = 0; k < locations.length; k++) {
+
+        //Get index of the location
+        if (location == locations[k][0]) {
+          index = k;
+        }
+      }
+
+      //If the index is -1, it is not in the locations array
+      if (index === -1) {
+
+        //Push the location into the locations array
+        locations.push(location);
+        index = k;
+
+        //If this is not the first day, add zeroes to number of cases to the preceding days
+        if (i != 0) {
+          for (var l = 0; l < i; l++) {
+            locations[index].push(0);
+          }
+        } 
+      }
+
+      //Push the weight 
+      locations[index].push(weight);
+    }
+  }
+
+  return locations;
+}
+
+/**
+ * Adds markers and event listeners to locations with cases
+ * @param days_array, array of days with information about locations
+ *      and number of cases
+ * @param day, selected day [day 0 - 18]
+ * @param location_data, 2d array of locations and their respective number of cases
+ *      in the span of 19 days
+ */
+function addMarker(days_array, day, location_data) {
+  var selected_day = days_array[day]
+
+  var infoWindow;
+  var marker;
+  var marker_data;
+
+  //Add markers for every location for the selected day
+  for (var i = 0; i < selected_day.length; i++) {
+    marker = new google.maps.Marker({
+      position: selected_day[i]['location'],
+      map,
+      icon:  {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 10,
+        strokeOpacity: 0,
+      },
+    });
+    
+    infoWindow  = new google.maps.InfoWindow();
+
+    var location_string = selected_day[i]['location'].lat() + " " + selected_day[i]['location'].lng();
+
+    //For every location in the location data, check if the marker location is the same
+    for (var k = 0; k < location_data.length; k++) {
+
+      //If the marker location string is equal to the location data 
+      if (location_string === location_data[k][0]) {
+        marker_data = [];
+
+        //Generate marker data
+        for (var l = 1; l <= days_array.length; l++) {
+          var day = new Date((l - 10) * MILLISECONDS_IN_A_DAY + (DATE_TODAY.getTime()))
+          var date = (day.getMonth() + 1) + "/" + day.getDate();
+          marker_data.push([date, parseInt(location_data[k][l])]);
+        }
+      }
+    }
+
+    marker.data = marker_data;
+
+    //Listener if a marker is clicked
+    google.maps.event.addListener(marker, 'click', function() {
+      drawLineChart(this, infoWindow);
+    });
+  }
+}
+
+/**
+ * Draw line chart inside an info window
+ * @param marker
+ * @param infoWindow 
+ */
+function drawLineChart(marker, infoWindow) {
+  
+  // Create the data table.
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Day');
+  data.addColumn('number', 'Number of Cases');
+
+  data.addRows(marker.data);
+
+  var options = {title:'Location: ' + marker.getPosition().toString() + "\n" +
+                      slider_selected_date + ": " + marker.data[slider_value][1],
+                  width:600,
+                  height:250,
+                  legend:'none',
+                  colors: ['#b36a5e'],
+                  pointsVisible: true,
+                  hAxis: {
+                    title: "Date",
+                    showTextEvery: 1,
+                    slantedText: true,
+                  },
+                  vAxis: {
+                    title: "Number of Cases",
+                  },
+                };
+                 
+  var node = document.createElement('div'),
+            infoWindow,
+            chart = new google.visualization.LineChart(node);
+      
+  infoWindow.setContent(node);
+  infoWindow.setOptions({maxWidth:600})
+  infoWindow.open(marker.getMap(),marker);
+  google.maps.event.addListener(infoWindow, 'domready', function () {
+    chart.draw(data, options);
+  });
+}
+
+/**
+ * Creates the heatmap layer
+ *  @param day, day 0 - 18; Day used in getting the heatmap points
+ */
+function initHeatMap(day) {
+  (async () => {
+    
+    //Fetch all the data
+    var days_array = await getData();
+    var location_data = getDataPerLocation(days_array);
+    var total_number_array = getTotalNumberArray(days_array);
+    
+    //Initialize the heatmap layer 
+    heatmap = new google.maps.visualization.HeatmapLayer({
+      data: days_array[day], 
+      opacity: 0.5,
+      map: map,
+      radius: 3,
+      maxIntensity: 20000,
+      dissipating: false,
+    });
+
+    var slider = document.getElementById("dateSlider");
+    var date_output = document.getElementById("selectedDate");
+    var total_number_of_cases = document.getElementById("totalNumber");
+
+    //Display default date
+    slider_value = 9;
+    slider_selected_date = MONTHS[DATE_TODAY.getMonth()] + " " + DATE_TODAY.getDate() + ", " + DATE_TODAY.getFullYear()
+    date_output.innerHTML = slider_selected_date;
+
+    //Data for the selected day
+    var selected_day = days_array[0];
+
+    //Invoke function to display the total number of cases for default day
+    total_number_of_cases.innerHTML = total_number_array[9];
+    
+    //Invoke function to add markers to the heatmap
+    addMarker(days_array, 0, location_data);
+
+    //Event when slider input changes
+    slider.oninput = function() {
+        slider_value = this.value;
+        var selected_date = new Date((slider_value - 9) * MILLISECONDS_IN_A_DAY + (DATE_TODAY.getTime()));
+
+        //Update date
+        slider_selected_date = MONTHS[selected_date.getMonth()] + " " + selected_date.getDate() + ", " + selected_date.getFullYear()
+        date_output.innerHTML = slider_selected_date;
+
+        //Update heatmap date
+        selected_day = days_array[slider_value];
+        heatmap.setData(selected_day);
+
+        //Update total number of cases
+        total_number_of_cases.innerHTML = total_number_array[slider_value];
+        
+    }
+  })(); 
+}
+
+/**
  * Initializes the google map
  */
-function initMap() {
+ function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
       zoom: 2.3,
       center: { lat: 37.774546, lng: -122.433523 },
+      streetViewControl: false,
+      mapTypeControl: true,
+      gestureHandling: "greedy",
+      minZoom: 2.2,
       styles: [
           { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
           { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
@@ -126,208 +366,7 @@ function initMap() {
             stylers: [{ color: "#17263c" }],
           },
         ],
-      streetViewControl: false,
-      mapTypeControl: true,
-      gestureHandling: "cooperative",
-      minZoom: 2.2,
   });
 
-  /** FOR LEGEND
-  const legend = document.getElementById("legend");
-  const div = document.createElement("div");
-  div.innerHTML = 'legend here'
-  // example'<img src="' + icon + '"> ';
-  legend.appendChild(div);
-
-  map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(legend);
-   */
   initHeatMap(9);
-}
-
-/**
- * Computes the total number of cases in a day
- * @param selected_day, selected day [day 0 - 9]
- * @param total_number
- */
-function getTotalNumber(selected_day, total_number) {
-  var total_number_of_cases = 0;
-
-  for (var i = 0; i < selected_day.length; i++) {
-    total_number_of_cases += parseFloat(selected_day[i]['weight']);
-  }
-  total_number.innerHTML = total_number_of_cases;
-}
-
-function getDataPerLocation(days_array) {
-
-  var location;
-  var weight;
-  var locations = [];
-
-  //Iterate through day 0 to 9
-  for (var i = 0; i < days_array.length; i++) {
-
-    //Iterate through every location
-    for (var j = 0; j < days_array[i].length; j++) {
-      location = [days_array[i][j]['location'].lat() + " " + days_array[i][j]['location'].lng()];
-      weight = days_array[i][j]['weight'];
-      var index = -1;
-      var k;
-
-      //Check if location is in locations array
-      for (k = 0; k < locations.length; k++) {
-        if (location == locations[k][0]) {
-          index = k;
-        }
-      }
-
-      if (index === -1) {
-        locations.push(location);
-        index = k;
-      }
-
-      locations[index].push(weight);
-    }
-  }
-  return locations;
-}
-
-/**
- * Adds markers and event listeners to locations with cases
- * @param selected_day, selected day [day 0 -9]
- */
-function addMarker(days_array, day) {
-  var selected_day = days_array[day]
-
-  //Array is arranged such that every index is [location weight1, weight2 ...]
-  var location_data = getDataPerLocation(days_array);
-  var infoWindow;
-  var marker;
-  var marker_data;
-
-  //Add markers for every location for the selected day
-  for (var i = 0; i < selected_day.length; i++) {
-    marker = new google.maps.Marker({
-      position: selected_day[i]['location'],
-      map,
-      icon:  {
-        path: google.maps.SymbolPath.CIRCLE,
-        scale: 10,
-        strokeOpacity: 0,
-      },
-    });
-    
-    infoWindow  = new google.maps.InfoWindow();
-
-    var location_string = selected_day[i]['location'].lat() + " " + selected_day[i]['location'].lng();
-
-    //For every location in the location data, check if the marker location is the same
-    for (var k = 0; k < location_data.length; k++) {
-
-      //If the marker location string is equal to the location data 
-      if (location_string === location_data[k][0]) {
-        marker_data = [];
-
-        //Generate marker data
-        for (var l = 1; l <= days_array.length; l++) {
-          var day = new Date((l - 10) * MILLISECONDS_IN_A_DAY + (DATE_TODAY.getTime()))
-          var date = MONTHS[day.getMonth()] + " " + day.getDate() + ", " + day.getFullYear()
-          marker_data.push([date, parseInt(location_data[k][l])]);
-        }
-      }
-    }
-
-    marker.data = marker_data;
-
-    google.maps.event.addListener(marker, 'click', function() {
-      drawLineChart(this, infoWindow);
-    });
-  }
-}
-
-function drawLineChart(marker, infoWindow) {
-  
-  // Create the data table.
-  var data = new google.visualization.DataTable();
-  data.addColumn('string', 'Day');
-  data.addColumn('number', 'Number of Cases');
-
-  data.addRows(marker.data);
-
-  var options = {title:'Location: ' + marker.getPosition().toString(),
-                  width:700,
-                  height:220,
-                  legend:'none',
-                  colors: ['#b36a5e'],
-                  hAxis: {
-                    showTextEvery: 2,}
-                };
-                 
-  var node = document.createElement('div'),
-            infoWindow,
-            chart = new google.visualization.LineChart(node);
-      
-  infoWindow.setContent(node);
-  infoWindow.open(marker.getMap(),marker);
-  google.maps.event.addListener(infoWindow, 'domready', function () {
-    chart.draw(data, options);
-  });
-}
-
-/**
- * Creates the heatmap layer
- *  @param day, day 0 - 9; Day used in getting the heatmap points
- */
-function initHeatMap(day) {
-  (async () => {
-    
-    //Fetch all the data
-    var days_array = await getData();
-    
-    //Initialize the heatmap layer 
-    heatmap = new google.maps.visualization.HeatmapLayer({
-      data: days_array[day], 
-      opacity: 0.5,
-      map: map,
-      radius: 3,
-      maxIntensity: 20000,
-      dissipating: false,
-    });
-
-    var slider = document.getElementById("dateSlider");
-    var date_output = document.getElementById("selectedDate");
-    var total_number = document.getElementById("totalNumber");
-
-    //Display default date
-    date_output.innerHTML = MONTHS[DATE_TODAY.getMonth()] + " " + DATE_TODAY.getDate() + ", " + DATE_TODAY.getFullYear();
-
-    //Data for the selected day
-    var selected_day = days_array[0];
-
-    //Invoke function to display the total number of cases for default day
-    getTotalNumber(selected_day, total_number);
-    
-    //Invoke function to add markers to the heatmap
-    addMarker(days_array, 0);
-
-    //Event when slider input changes
-    slider.oninput = function() {
-
-        var selected_date = new Date((this.value - 9) * MILLISECONDS_IN_A_DAY + (DATE_TODAY.getTime()));
-
-        //Update date
-        date_output.innerHTML = MONTHS[selected_date.getMonth()] + " " + selected_date.getDate() + ", " + selected_date.getFullYear();
-
-        selected_day = days_array[this.value];
-        
-        //Update heatmap date
-        heatmap.setData(selected_day);
-
-        //Update total number of cases
-        getTotalNumber(selected_day, total_number);
-        
-        //Update markers
-        addMarker(days_array, this.value);
-    }
-  })(); 
 }
